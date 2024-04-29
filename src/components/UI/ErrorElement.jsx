d427bcc432cf8ebd60429c4abd7865fc6cb989fb
Ramy <ramyalsabry.dev@gmail.com>
2024-04-29 23:27:59 +0300

    some enhance

--- src/components/UI/ErrorElement.jsx
+++ src/components/UI/ErrorElement.jsx
@@ -7,7 +7,6 @@ import { useRouteError } from "react-router-dom";
  */
 const ErrorElement = () => {
   const error = useRouteError();
-  console.log("🚀 ~ file: ErrorElement.jsx:5 ~ ErrorElement ~ error:", error);
 
   return <h1 className="text-4xl font-bold">There was an error...</h1>;
 };
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 
 const Cart = () => {
   const user = useSelector((state) => state.user.user);
-  console.log("🚀 ~ Cart ~ user:", user);
   const numItemsInCart = useSelector((state) => state.cart.numItemsInCart);
 
   if (numItemsInCart === 0) {
